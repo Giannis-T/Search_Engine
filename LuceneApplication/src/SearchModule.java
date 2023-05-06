3c69fcbd66cc256923ecc973262ae08ef69cf43e
Giannis-T <giannis-t@gmail.com>
2023-05-06 18:59:09 +0300

    Lucene Application with Highlight feature

--- LuceneApplication/src/SearchModule.java
+++ LuceneApplication/src/SearchModule.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -22,16 +24,27 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.search.highlight.Fragmenter;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
+import org.apache.lucene.search.highlight.TextFragment;
+import org.apache.lucene.search.highlight.TokenSources;
 
 import com.opencsv.CSVReader;
 
 import org.apache.lucene.store.ByteBuffersDirectory;
-import java.util.Scanner;  
 
+import java.util.Formatter;
+import java.util.Scanner;
 
 
 public class SearchModule {
-	public static void main(String[] args) throws IOException, ParseException {
+	
+	
+	public static void main(String[] args) throws IOException, ParseException, InvalidTokenOffsetsException {
 		StandardAnalyzer analyzer = new StandardAnalyzer();
 		
     	String querystr = args[0];
@@ -45,22 +58,39 @@ public class SearchModule {
 		
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("/SearchEngine/NewIndex")));
 		IndexSearcher searcher = new IndexSearcher(reader);
-		
-	    // Create a TopDocsCollector to retrieve the top hits
 	    TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage * pageNumber, Integer.MAX_VALUE);
-		// Execute the search and collect the top hits
-		searcher.search(q, collector);
-		// Retrieve the top hits for the current page
-	    ScoreDoc[] hits = collector.topDocs((pageNumber - 1) * hitsPerPage, hitsPerPage).scoreDocs;
+	  
 	    
+	    SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter();	    
+	    QueryScorer queryScorer = new QueryScorer(q);
+	    Highlighter highlighter = new Highlighter(htmlFormatter, queryScorer);
+	    highlighter.setTextFragmenter(new SimpleSpanFragmenter(queryScorer, Integer.MAX_VALUE));
+	    highlighter.setMaxDocCharsToAnalyze(Integer.MAX_VALUE);
+//	    
+        searcher.search(q, collector);
+        
+	    ScoreDoc[] hits = collector.topDocs((pageNumber - 1) * hitsPerPage, hitsPerPage).scoreDocs;
+
 		// 4. display results
 		System.out.println("Found " + hits.length + " hits.");
 		for(int i=0;i<hits.length;++i) {
 		    int docId = hits[i].doc;
 		    Document d = searcher.doc(docId);
-		    System.out.println("###"+(i + 1) + "$$$" + d.get("artist") + "$$$" + d.get("title")+ "$$$" + d.get("lyrics"));		
-		}  	
-		
+		    if (field.equals("artist")) {
+			    System.out.println("###"+(i + 1) + "$$$" + (highlighter.getBestFragment(analyzer, field, d.get(field))) + "$$$" + d.get("title")+ "$$$" + d.get("lyrics"));
+		    }
+		    else if (field.equals("title")){
+			    System.out.println("###"+(i + 1) + "$$$" + d.get("artist") + "$$$" + (highlighter.getBestFragment(analyzer, field, d.get(field)))+ "$$$" + d.get("lyrics"));
+
+		    }
+		    else {
+			    System.out.println("###"+(i + 1) + "$$$" + d.get("artist") + "$$$" + d.get("title")+ "$$$" + (highlighter.getBestFragment(analyzer, field, d.get(field))));
+
+		    }
+//		    System.out.println("###"+(i + 1) + "$$$" + d.get("artist") + "$$$" + d.get("title")+ "$$$" + d.get("lyrics"));
+             
+		}
+			
 		reader.close();
 	}		
 }
